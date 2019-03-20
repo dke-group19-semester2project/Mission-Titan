c9620155e3069e892de94bc3683485269b103b07
LauraIst <43955416+LauraIst@users.noreply.github.com>
2019-03-20 23:31:06 +0100

    Double-checking G in au

--- JavaFXTest.java
+++ JavaFXTest.java
@@ -5,6 +5,7 @@ import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
@@ -14,53 +15,69 @@ import javafx.scene.*;
 import javafx.util.Duration;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class JavaFXTest extends Application {
+    private double WIDTH = 1000;
+    private double HEIGHT = 1000;
     public void start(Stage primaryStage) throws Exception {
+
+
+
+        double planetAUToPixelsMultiplier = Math.pow(10, 6)/2;
+        double sunAUToPixelsMultiplier = Math.pow(10,4)/2;
+        double distanceToPixelsMultiplier = (Math.pow(10,3));
         long startTime = System.currentTimeMillis();
         Sphere sun = new Sphere();
-        sun.setTranslateX(300.0f);
-        sun.setTranslateY(300.0f);
-        sun.setRadius(16.0f);
+        sun.setTranslateX(0);
+        sun.setTranslateY(0);
+        sun.setRadius(0.00465*sunAUToPixelsMultiplier); //Based on sun radius (convert 1391000/2 km (kilometers) to astronomical units)×10^5/5
 
-        Sphere earth = new Sphere();
-        earth.setTranslateX(400);
-        earth.setTranslateX(400);
-        earth.setRadius(8);
-        Sphere mercury = new Sphere ();
-        mercury.setTranslateX(250);
-        mercury.setTranslateY(250);
-        mercury.setRadius(4.0f);
+        // Mercury position on A.D. 2019-Mar-17 00:00:00
+        double mercuryStartX = -3.884951255359090E-01;
+        double mercuryStartY = -1.193703739273220E-02;
+        double mercuryStartZ = 3.466414962075259E-02;
+        Planet mercury = new Planet(3.302*10e23, mercuryStartX, mercuryStartY, mercuryStartZ,  (-4.989539396985841E-03/86400), (-2.691230619778414E-02/86400), (-1.741371578050843E-03/86400));
+        Sphere mercurySphere = new Sphere();
+        mercurySphere.setRadius(1.6308E-5*planetAUToPixelsMultiplier);
+        mercurySphere.setTranslateX(mercuryStartX*distanceToPixelsMultiplier);
+        mercurySphere.setTranslateY(mercuryStartY*distanceToPixelsMultiplier);
+        System.out.println(mercurySphere.getRadius());
+        System.out.println(mercurySphere.getTranslateX());
+        System.out.println(mercurySphere.getTranslateY());
+        System.out.println();
 
-        PathTransition earthTransition = new PathTransition();
-        earthTransition.setDuration(Duration.millis(15000));
-        earthTransition.setNode(earth);
-        earthTransition.setPath(new Ellipse(sun.getTranslateX(), sun.getTranslateY(), 50, 200));
-        // TODO: Would need to know the angle of the elliptical orbit of each planet in relation to our absolute X and Y axes
-        earthTransition.setCycleCount(Animation.INDEFINITE);
-        earthTransition.setInterpolator(Interpolator.LINEAR);
-        earthTransition.play();
+        Polygon polygon = new Polygon();
+        Double[] mercuryTrajectory = mercury.getTrajectoryPoints(88);
+        polygon.getPoints().addAll(mercuryTrajectory);
 
-        PathTransition mercuryTransition = new PathTransition();
-        mercuryTransition.setDuration(Duration.millis(10000));
-        mercuryTransition.setNode(mercury);
-        mercuryTransition.setPath(new Ellipse(sun.getTranslateX(), sun.getTranslateY(), 50, 40));
-        mercuryTransition.setCycleCount(Animation.INDEFINITE);
-        mercuryTransition.setInterpolator(Interpolator.LINEAR);
-        mercuryTransition.play();
+        PathTransition mercuryOrbit = new PathTransition();
+        mercuryOrbit.setDuration(Duration.millis(15000));
+        mercuryOrbit.setNode(mercurySphere);
+        mercuryOrbit.setPath(polygon);
+        mercuryOrbit.setInterpolator(Interpolator.LINEAR);
+        mercuryOrbit.setCycleCount(Animation.INDEFINITE);
+        mercuryOrbit.play();
 
-        Group root = new Group(sun, earth, mercury);
-        Scene scene = new Scene(root,600, 600);
+        Group root = new Group(sun, mercurySphere);
+        Scene scene = new Scene(root,WIDTH, HEIGHT);
+
+        Camera camera= new PerspectiveCamera();
+        camera.translateXProperty().set(-WIDTH/2);
+        camera.translateYProperty().set(-HEIGHT/2);
+        camera.translateZProperty().set(-1000);
+        scene.setCamera(camera);
+
         primaryStage.setTitle("Test");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-
 
     }
     public static void main(String args[]){
         launch(args);
     }
+
 }
---
+++ Planet.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Planet {
+    private int secondsPerDay = 86400;
+    private double xPosition; // when creating an object
+    private double yPosition; // take initial position and velocity
+    private double zPosition; // from the same date for every planet
+    private double xVelocity; // for example, 01-01-2000
+    private double yVelocity;
+    private double zVelocity;
+    private double mass;
+    private final double G = 1.9935238E-44;//6.67408*10e-11; // the commented-out number is with m3 rather than au3
+    private final double M = 2*10e30;
+
+    public Planet (double mass, double xPosition, double yPosition, double zPosition, double xVelocity, double yVelocity, double zVelocity) {
+        this.mass = mass;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.zPosition = zPosition;
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
+        this.zVelocity = zVelocity;
+    }
+
+    public double xPosition(double time) {
+        double r = Math.sqrt(xPosition*xPosition + yPosition*yPosition + zPosition*zPosition);
+        double force = (G*M)/(r*r);
+        xPosition = xPosition + (time * (xVelocity + (force/this.mass)*time));
+        xVelocity = xVelocity + ((force/this.mass)*time);
+        return xPosition;
+    }
+    public double yPosition(double time) {
+        double r = Math.sqrt(xPosition*xPosition + yPosition*yPosition + zPosition*zPosition);
+        double force = (G*M)/(r*r);
+        yPosition = yPosition + (time * (yVelocity + (force/this.mass)*time));
+        yVelocity = yVelocity + ((force/this.mass)*time);
+        return yPosition;
+    }
+    public double zPosition(double time) {
+        double r = Math.sqrt(xPosition*xPosition + yPosition*yPosition + zPosition*zPosition);
+        double force = (G*M)/(r*r);
+        zPosition = zPosition + (time * (zVelocity + (force/this.mass)*time));
+        zVelocity = zVelocity + ((force/this.mass)*time);
+        return zPosition;
+    }
+
+    public static void main (String args[]) {
+        int secondsPerDay = 86400;
+        Planet mercury = new Planet(3.302*10e23, -3.884951255359090E-01,
+                -1.193703739273220E-02, 3.466414962075259E-02,
+                (-4.989539396985841E-03/86400), (-2.691230619778414E-02/86400),
+                (-1.741371578050843E-03/86400));
+        mercury.getTrajectoryPoints(88);
+//        System.out.println(mercury.xPosition);
+//        System.out.println(mercury.yPosition);
+//        System.out.println(mercury.zPosition);
+//        for (int i = 0; i<86400; i++) {
+//            mercury.xPosition(1);
+//            mercury.yPosition(1);
+//            mercury.zPosition(1);
+//        }
+//        System.out.println(mercury.xPosition);
+//        System.out.println(mercury.yPosition);
+//        System.out.println(mercury.zPosition);
+//        for (int i = 0; i<30; i++) {
+//            mercury.xPosition(secondsPerDay);
+//            mercury.yPosition(secondsPerDay);
+//            mercury.zPosition(secondsPerDay);
+//            System.out.println(mercury.xPosition);
+//            System.out.println(mercury.yPosition);
+//            System.out.println(mercury.zPosition);
+//        }
+
+    }
+    public Double[] getTrajectoryPoints (int daysPerYear) {
+        double distanceToPixelsMultiplier = (Math.pow(10,2));
+        Double[] points = new Double[2*daysPerYear];
+        for (int i = 0; i<(2*daysPerYear); i=i+2) {
+            this.xPosition(secondsPerDay);
+            this.yPosition(secondsPerDay);
+            this.zPosition(secondsPerDay);
+            System.out.println("Round " + i/2 + " New X position: " + xPosition);
+            System.out.println("Round " + i/2 + " New Y position: " + yPosition);
+            points[i] = xPosition*distanceToPixelsMultiplier;
+            points[i+1] = yPosition*distanceToPixelsMultiplier;
+        }
+        return points;
+    }
+}
